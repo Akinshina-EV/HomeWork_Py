78df0390affd832597b30d2add76626d65f01e65
Akinshina-EV <akinshinaev86@gmail.com>
2022-10-04 23:42:57 +0300

    Changes by kairick

--- file_def.py
+++ file_def.py
@@ -1,9 +1,16 @@
-def get_list_int():
-    input_lst = input(
-        'Введите список, состоящий из нескольких натуральных чисел:\n').split()
-    result_list = []
-    for i in input_lst:
-        result_list.append(int(i))
+def get_list(mark=True):
+    number_type = '' if mark else 'вещественных'
+    count = int(input(f'Задайте длину списка, состоящего из {number_type} '
+                      f'чисел: '))
+    input_lst = []
+    for i in range(1, count + 1):
+        input_number = input(
+            f'Введите {i} {number_type} элемент списка: ')
+        input_lst.append(input_number)
+    if mark:
+        result_list = [int(i) for i in input_lst]
+    else:
+        result_list = [float(i) for i in input_lst]
     print(f'Исходный список:\n{result_list}')
     return result_list
 
